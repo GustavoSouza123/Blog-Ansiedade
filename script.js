92a23d4596097226cf374ae085fd542fd94f96bd
Gustavo Souza <gustavoelia7@gmail.com>
2023-04-24 10:51:58 -0300

    ultimo commit no lab

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const c = (e) => document.querySelector(e);
 const cs = (e) => document.querySelectorAll(e);
 
-c('nav .menu-toggle').onclick = () => {
+c('.menu-toggle').onclick = () => {
     cs('nav .menu-toggle span')[0].classList.toggle('active');
     cs('nav .menu-toggle span')[1].classList.toggle('active');
     cs('nav .menu-toggle span')[2].classList.toggle('active');
+
+    c('nav').classList.toggle('active');
+    c('nav header').classList.toggle('active');
+    c('nav ul').classList.toggle('active');
+    c('nav .menu-toggle').classList.toggle('active');
 }
