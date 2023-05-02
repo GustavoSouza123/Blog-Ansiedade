3ec61e994fe8a82ce53933dae80f40c183c8630a
Gustavo Souza <gustavoelia7@gmail.com>
2023-05-02 09:13:35 -0300

    abreviação do menu toggle

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const c = (e) => document.querySelector(e);
 const cs = (e) => document.querySelectorAll(e);
 
-c('.menu-toggle').onclick = () => {
-    cs('nav .menu-toggle span')[0].classList.toggle('active');
-    cs('nav .menu-toggle span')[1].classList.toggle('active');
-    cs('nav .menu-toggle span')[2].classList.toggle('active');
+window.onload = () => {
+    c('nav').classList.add('active');
+}
 
+c('.menu-toggle').onclick = () => {
     c('nav').classList.toggle('active');
-    c('nav header').classList.toggle('active');
-    c('nav ul').classList.toggle('active');
-    c('nav .menu-toggle').classList.toggle('active');
 }
