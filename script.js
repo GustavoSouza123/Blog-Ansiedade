e2e8e25675da84c3e45ac78d05eb1b4fd78532b0
Gustavo Souza <gustavoelia7@gmail.com>
2023-05-02 09:35:28 -0300

    modificações na navbar

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const cs = (e) => document.querySelectorAll(e);
 
 window.onload = () => {
     c('nav').classList.add('active');
+    c('nav .top').classList.add('active');
 }
 
 c('.menu-toggle').onclick = () => {
     c('nav').classList.toggle('active');
+    c('nav .top').classList.toggle('active');
+
+    cs('nav .menu-toggle span')[0].classList.toggle('active');
+    cs('nav .menu-toggle span')[1].classList.toggle('active');
+    cs('nav .menu-toggle span')[2].classList.toggle('active');
 }
